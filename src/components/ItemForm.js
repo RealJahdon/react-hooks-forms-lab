99db3e9e47aa3752c1fd54e6104b0b60ee84b7f3
Jahdon Ervin <Jahdonervin@gmail.com>
2023-03-22 21:21:40 -0400

    commit tree 5a2b8c174747ea95933b1a24f0e4f0a1db4fb335

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,23 +1,34 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, {useState} from "react";
+import {v4 as uuid} from "uuid";
 
-function ItemForm(props) {
+function ItemForm( props ) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onItemFormSubmit({
+      id: uuid(),name, category
+    });
+  }
+
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-
-      <label>
-        Category:
-        <select name="category">
+    <form className="NewItem" onSubmit={handleSubmit}>
+      <label>Name:<input type="text" name="name" value={name} onChange={handleNameChange}/></label>
+      <label>Category:<select name="category" value={category} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
-
       <button type="submit">Add to List</button>
     </form>
   );
